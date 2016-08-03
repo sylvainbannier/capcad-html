8fa2e2a210e4596d50c53baf6ddc950edaf07d34
Sylvain BANNIER <sylvain.bannier@smile.fr>
2016-08-03 23:25:44 +0200

    next features for ADD_IDEA

--- test/reducer_ADD_IDEA.spec.js
+++ test/reducer_ADD_IDEA.spec.js
@@ -5,17 +5,16 @@ const reducer = getReducer(() => "random_id");
 
 describe("reduce", () => {
   describe("ADD_IDEA", () => {
-    it('saves added idea to history', () => {
-      const initial_state = fromJS({
-        events: []
-      });
+    it('saves added idea event to history', () => {
+      const initial_state = fromJS({ events: [] });
       const action = {type: 'ADD_IDEA', entry: 'my new idea', id: 'random_id'};
-
       const nextState = reducer(initial_state, action);
-
       const expectedState = initial_state.updateIn(['events'], arr => arr.push(fromJS(action)));
       expect(nextState).to.equal(expectedState);
-    })
+    });
+
+    it('adds idea to the idea list');
+    it('opens the created idea');
   });
 });
 
