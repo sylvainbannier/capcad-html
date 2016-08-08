e142786120238a1529035fa33b53c8a778e63939
Sylvain BANNIER <sylvain.bannier@smile.fr>
2016-08-08 17:53:58 +0200

    test ADD_IDEA action

---
+++ test/action_ADD_IDEA.spec.js
@@ -0,0 +1,31 @@
+import { List, Map, fromJS } from 'immutable';
+import {expect} from 'chai';
+import {addIdeaFactory} from '../src/action_creators';
+import sinon from 'sinon';
+
+describe("action", () => {
+  describe("addIdea", () => {
+    let mock = { }
+    beforeEach(() => {
+      mock.randomIdGenerator = () => "random_id"
+      mock.browserHistory = {
+        push: sinon.spy()
+      };
+    });
+
+    it('saves added idea event to history', () => {
+      const action = addIdeaFactory(mock.randomIdGenerator,mock.browserHistory)("text");
+      expect(action).to.eql({
+        type:"ADD_IDEA",
+        entry: "text",
+        id: "random_id"
+      })
+    });
+
+    it('changes URL to /idea/random_id', ()=> {
+      const action = addIdeaFactory(mock.randomIdGenerator, mock.browserHistory)("text");
+      expect(mock.browserHistory.push.called).to.be.ok
+    });
+  });
+});
+
