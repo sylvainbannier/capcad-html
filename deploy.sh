b78c1d70c7ee5630215a2b2e1db0a9fe7092deb3
Sylvain BANNIER <sylvain.bannier@smile.fr>
2016-09-13 12:06:37 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+npm build
+git checkout -B gh-pages
+git add -f build
+git commit -am "Rebuild website"
+git filter-branch -f --prune-empty --subdirectory-filter build
+git push -f origin gh-pages
+git checkout -
+
